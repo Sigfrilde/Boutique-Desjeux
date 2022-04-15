a15650c5b4d7ff6d18ae7d65109371b0e5586a22
Florian <florian.bachelet2@gmail.com>
2022-04-15 23:34:49 +0200

    commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+// Permet de savoir s'il y a une session. 
+// C'est-à-dire si un utilisateur s'est connecté à votre site 
+session_start();
+
+
+// Fichier PHP contenant la connexion à votre BDD
+include('bd/connexionDB.php');
